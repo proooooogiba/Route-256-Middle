2a6c0330c41a1bd17b73c19bf841a472c1e14376
Ivan Pogiba <ipogiba@ozon.ru>
2024-06-20 23:54:56 +0300

    [HW-3] swagger is set up

--- loms/internal/mw/cors.go
+++ loms/internal/mw/cors.go
@@ -7,8 +7,8 @@ import (
 func WithHTTPCorsMiddleware(next http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding")
+		w.Header().Set("Access-Control-Allow-Methods", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
 			return
